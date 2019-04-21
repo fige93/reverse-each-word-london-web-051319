99d504d43c672ea0f302d29269f3d9b3f44218e8
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-21 16:55:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   reverseArray = []
   stringArray = string.split()
   stringArray.each do |x|
-    reverseArray << x.split("").reverse.join("").join("")
+    reverseArray << x.split("").reverse.join("")
   end
   reverseArray
 end
